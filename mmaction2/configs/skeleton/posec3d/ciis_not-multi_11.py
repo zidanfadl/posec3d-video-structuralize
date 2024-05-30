920360c0cec817e9531a0387f2c7f086b06cea13
Aldy Raja Sipahutar <56700288+aldyraja@users.noreply.github.com>
2024-05-30 12:37:28 +0700

    Update ciis_not-multi_11.py

--- mmaction2/configs/skeleton/posec3d/ciis_not-multi_11.py
+++ mmaction2/configs/skeleton/posec3d/ciis_not-multi_11.py
@@ -246,7 +246,7 @@ param_scheduler = [
         #     AmpOptimizerWrapper. See :meth:`build_optim_wrapper` for
         #     examples. Defaults to None.
 optim_wrapper = dict(
-    optimizer=dict(type='SGD', lr=0.2, momentum=0.9, weight_decay=0.0001),
+    optimizer=dict(type='SGD', lr=0.075, momentum=0.9, weight_decay=0.0001),
     clip_grad=dict(max_norm=40, norm_type=2))
 
 
